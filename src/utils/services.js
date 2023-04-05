18e4b587d319392491ab1d24012b0f6a8802fbff
pooyadhgh <pooya.dhgh@gmail.com>
2023-04-05 20:38:27 +0200

    Modify api

--- src/utils/services.js
+++ src/utils/services.js
@@ -12,15 +12,17 @@ export const getAIResponse = async (promptType, text) => {
     "Content-Type": "application/json",
     Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
   };
+
   const body = JSON.stringify({
     prompt,
     model: "text-davinci-003",
-    temperature: 0.5,
-    max_tokens: 60,
+    temperature: 0.8,
+    max_tokens: 300,
     top_p: 1.0,
     frequency_penalty: 0.8,
     presence_penalty: 0.0,
   });
+
   const options = {
     method: "POST",
     headers,
